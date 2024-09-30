98758d17ca6f944b777bc08a4f71bcf73469437a
Choi5903 <cmg9431@gmail.com>
2024-09-30 14:38:01 +0900

    [Feat] Routes 경로 설정

---
+++ Node/Routes/PlayerRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');       //fs 모듈 추가
+const router = express.Router();
+
+//초기 지원 설정
+const initalResources = 
+{
+    meta : 500,
+    crystal : 300,
+    deuterirum : 100,
+}
+
+//글로벌 플레이어 객체 초기화
+global.players = {};            //글로벌 객체 초기화
+
+
+
+module.exports = router;        //라우터 내보내기
